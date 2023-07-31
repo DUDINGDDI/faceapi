39b2fb0c0d146d7569698f4bf580b51b213c271b
김민식 <sakmsik98@naver.com>
2023-07-31 15:28:46 +0900

    face

--- src/App.js
+++ src/App.js
@@ -36,20 +36,35 @@ function App() {
       faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
       faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
       faceapi.nets.tinyYolov2.loadFromUri('/models')
-    
+
     ]).then(() => {
       faceMyDetect()
     })
   }
 
-  const faceMyDetect = () => {
-    setInterval(async () => {
-      const detectionWithExpressions = await faceapi.detectSingleFace(videoRef.current).withFaceLandmarks().withFaceExpressions()
-      console.log(detectionWithExpressions);
+const faceMyDetect = () => {
+  setInterval(async () => {
+    const displaySize = { width: videoRef.current.width, height: videoRef.current.height }
 
+    // 입력 차원에 맞게 오버레이 캔버스의 크기 조정
+    const canvas = canvasRef.current;
+    faceapi.matchDimensions(canvas, displaySize);
 
-    },1000)
-  }
+    const detectionsWithExpressions = await faceapi
+      .detectAllFaces(videoRef.current)
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    console.log(detectionsWithExpressions);
+
+    // 표시된 이미지와 원본 이미지의 크기가 다를 경우 감지된 상자와 랜드마크 크기 조정
+    const resizedResults = faceapi.resizeResults(detectionsWithExpressions, displaySize);
+    // 캔버스에 감지 결과 그리기
+    faceapi.draw.drawDetections(canvas, resizedResults);
+    // 최소 확률로 얼굴 표현을 나타내는 텍스트 상자 그리기
+    const minProbability = 0.05;
+    faceapi.draw.drawFaceExpressions(canvas, resizedResults, minProbability);
+  }, 1000);
+};
 
   return (
     <div className="myapp">
