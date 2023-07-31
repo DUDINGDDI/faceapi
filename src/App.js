89e502b0f8ed3b0349b2271919392508a1b5bf1f
김민식 <sakmsik98@naver.com>
2023-07-31 15:20:32 +0900

    face api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from 'react'
+
+import * as faceapi from 'face-api.js'
+import * as canvas from 'canvas';
+
+const { Canvas, Image, ImageData } = canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
 
 function App() {
+  const videoRef = useRef()
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    startVideo()
+    videoRef && loadModels()
+
+  }, [])
+
+  //캠켜기
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((currentStream) => {
+      videoRef.current.srcObject = currentStream
+    })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  //faceapi
+  const loadModels = () => {
+    Promise.all([
+      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
+      faceapi.nets.mtcnn.loadFromUri('/models'),
+      faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.tinyYolov2.loadFromUri('/models')
+    
+    ]).then(() => {
+      faceMyDetect()
+    })
+  }
+
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      const detectionWithExpressions = await faceapi.detectSingleFace(videoRef.current).withFaceLandmarks().withFaceExpressions()
+      console.log(detectionWithExpressions);
+
+
+    },1000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myapp">
+      <h1>Face Detection</h1>
+      <div className="appvide">
+
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+      </div>
+      <canvas ref={canvasRef} width="940" height="650"
+        className="appcanvas" />
     </div>
-  );
+  )
 }
 
 export default App;
